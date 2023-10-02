2aeb106b55ed947c4fe4fd81c947f10de1683aa3
Hennadii Borysevych <genuch3@gmail.com>
2023-10-02 20:56:51 +0200

    feat:scructure of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Lounge from "./components/Lounge/Lounge";
+import Support from "./components/Support/Support";
+import Search from "./components/Search/Search";
+import Travelers from "./components/Travelers/Travelers";
+import Subscribers from "./components/Subscribers/Subscribers";
+import Info from "./components/Info/Info";
+const App = () => (
+  <div>
+    <Navbar />
+    <Home />
+    <Lounge/>
+    <Support/>
+    <Search/>
+    <Travelers/>
+    <Subscribers/>
+    <Info/>
+  </div>
+);
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
+export default App;
